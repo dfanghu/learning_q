3bf3d5823e6f690be5dfcdacb3578bfcf0e8af84
jd <dfanghu@gmail.com>
2023-12-04 16:18:41 +0800

    first impression

---
+++ code_kx_com/q/learn/install/try_your_first_expression.py
@@ -0,0 +1,15 @@
+from lib.array_utils import q_range
+from lib.sys_utils import q_exit_cli
+
+
+def all_tests():
+    test1()
+    test2()
+
+
+def test1():
+    assert r"til 6" == q_range(6)
+
+
+def test2():
+    assert r"\\" == q_exit_cli()
---
+++ main.py
@@ -0,0 +1,4 @@
+import code_kx_com.q.learn.install.try_your_first_expression as ex1
+
+if __name__ == '__main__':
+    ex1.all_tests()
